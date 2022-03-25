d2488a3eacc162a16ad3aad4c9c8f5d59349dfde
JoannaBusmail <joanna.busmail@gmail.com>
2022-03-25 17:56:45 +0100

    re estrucure css preview

--- src/js/color.js
+++ src/js/color.js
@@ -1,5 +1,8 @@
 'use strict';
-
+//constantes de los elementos html
 const paletteBlue = document.querySelector ('.js_button_paletteBlue');
 const paletteRed = document.querySelector ('.js_button_paletteRed');
 const paletteMix = document.querySelector ('.js_button_paletteMix');
+const nameAndLastname = document.querySelector('.js_names-container');
+const profession =document.querySelector ('.js_card__profession');
+const elementRectangle = document.querySelector ('.js_card__rectangle');
