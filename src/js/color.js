4054396bd6ee01e991cfd5b8a90c789970bf8421
JoannaBusmail <joanna.busmail@gmail.com>
2022-03-25 18:28:41 +0100

    branch joanna-js

--- src/js/color.js
+++ src/js/color.js
@@ -6,3 +6,19 @@ const paletteMix = document.querySelector ('.js_button_paletteMix');
 const nameAndLastname = document.querySelector('.js_names-container');
 const profession =document.querySelector ('.js_card__profession');
 const elementRectangle = document.querySelector ('.js_card__rectangle');
+const iconMobile = document.querySelector ('.js_iconMobile');
+const iconMail =document.querySelector('.js_iconMail');
+const iconLinkedIn = document.querySelector ('.js_iconLinkedIn');
+const iconGit = document.querySelector ('.js_iconGit')
+const containerMobile = document.querySelector ('.js_containerMobile');
+const containerMail = document.querySelector ('.js_containerMail');
+const containerLinkedIn = document.querySelector ('.js_containerLinkedIn');
+const containerGit = document.querySelector ('.js_containerGit');
+
+
+
+function colorChange () {
+nameAndLastname.classList.add ('newColor');
+}
+
+paletteRed.addEventListener ('click' , colorChange);
