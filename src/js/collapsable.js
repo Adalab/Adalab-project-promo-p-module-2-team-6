6351f91ce78ce56762a711a5ac8e6e19c5a1e1d6
PradoCarretero <p.carreteroayala@gmail.com>
2022-03-25 18:24:02 +0100

    js collapse

--- src/js/collapsable.js
+++ src/js/collapsable.js
@@ -1,3 +1,40 @@
 'use strict';
 
-console.log('holis');
+//--------CONST GENERAL-----------
+/* const legend */
+const fillLegend = document.querySelector('.js-filled');
+const designLegend = document.querySelector('.js-design');
+const shareLegend = document.querySelector('.js-share');
+/* const wrapper */
+const designWrapper = document.querySelector('.js-design__wrapper');
+const fillWrapper = document.querySelector('.js-filled__wrapper');
+const shareWrapper = document.querySelector('.js-share__wrapper');
+const shareWrapper2 = document.querySelector('.js-share__wrapper2'); 
+/* const triangle */
+const fillTriangle = document.querySelector('.js-fill__triangle');
+const designTriangle = document.querySelector('.js-design__triangle');
+const shareTriangle = document.querySelector('.js-share__triangle');
+
+
+//-------- share-----------            
+function handleArrowS(){
+shareWrapper.classList.toggle('hidden');
+shareWrapper2.classList.toggle('hidden');
+shareTriangle.classList.toggle('turn');
+}
+shareLegend.addEventListener('click', handleArrowS);
+
+//-------- fill-----------
+function handleArrowF(){
+fillWrapper.classList.toggle('hidden');
+fillTriangle.classList.toggle('turn');
+}
+fillLegend.addEventListener('click', handleArrowF);
+
+
+//-------- design-----------
+function handleArrowD(){
+designWrapper.classList.toggle('hidden');
+designTriangle.classList.toggle('turn');
+}
+designLegend.addEventListener('click', handleArrowD);
