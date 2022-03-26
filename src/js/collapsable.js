be61b3ae9d23f23524c5661398e0bc3052d4a0bc
JoannaBusmail <joanna.busmail@gmail.com>
2022-03-26 17:16:15 +0100

    fixing js namecard

--- src/js/collapsable.js
+++ src/js/collapsable.js
@@ -1,2 +1,11 @@
 'use strict';
 
+
+
+const inputName = document.querySelector('.js-inputName');
+const nameCard = document.querySelector ('.js-nameCard');
+
+inputName.addEventListener ('keyup', () =>{
+const name = inputName.value;
+nameCard.innerHTML = name;
+});
