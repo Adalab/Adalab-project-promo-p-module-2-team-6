6bff07e2f529170e09fd750f1bfac9bcca06d68c
PradoCarretero <p.carreteroayala@gmail.com>
2022-03-28 21:08:40 +0200

    collapsed

--- src/js/collapsable.js
+++ src/js/collapsable.js
@@ -9,49 +9,48 @@ const shareLegend = document.querySelector('.js-share');
 const designWrapper = document.querySelector('.js-design__wrapper');
 const fillWrapper = document.querySelector('.js-filled__wrapper');
 const shareWrapper = document.querySelector('.js-share__wrapper');
-const shareWrapper2 = document.querySelector('.js-share__wrapper2'); 
 /* const triangle */
 const fillTriangle = document.querySelector('.js-fill__triangle');
 const designTriangle = document.querySelector('.js-design__triangle');
 const shareTriangle = document.querySelector('.js-share__triangle');
-
-
+ 
+ 
 //-------- share-----------            
 function handleArrowS(){
 shareWrapper.classList.toggle('hidden');
-shareWrapper2.classList.toggle('hidden');
 designWrapper.classList.add('hidden');
 fillWrapper.classList.add('hidden');
-/* shareTriangle.classList.toggle('turn'); */
+shareTriangle.classList.toggle('turn'); 
 }
-
+ 
 shareLegend.addEventListener('click', handleArrowS);
-
-if (shareWrapper2.classList.contains('hidden')){
-    console.log(yeah);
-    shareTriangle.classList.add('turn');
-}
-else {
-    shareTriangle.classList.remove('turn');
-};
-
+ 
+ 
 //-------- fill-----------
 function handleArrowF(){
 fillWrapper.classList.toggle('hidden');
 designWrapper.classList.add('hidden');
 shareWrapper.classList.add('hidden');
-shareWrapper2.classList.add('hidden');
 fillTriangle.classList.toggle('turn');
 }
 fillLegend.addEventListener('click', handleArrowF);
-
-
+ 
 //-------- design-----------
 function handleArrowD(){
-designWrapper.classList.toggle('hidden');
-designTriangle.classList.toggle('turn');
-shareWrapper.classList.add('hidden');
-shareWrapper2.classList.add('hidden');
-fillWrapper.classList.add('hidden');
-}
-designLegend.addEventListener('click', handleArrowD);
+    designWrapper.classList.toggle('hidden');
+    shareWrapper.classList.add('hidden');
+    fillWrapper.classList.add('hidden');
+    if (shareWrapper.classList.contains('hidden')){
+        console.log("yeahhh");
+        shareTriangle.classList.add('turn');
+    }
+    else {
+        shareTriangle.classList.remove('turn');
+        console.log("ahora no");
+    }
+    
+    }
+    designLegend.addEventListener('click', handleArrowD);
+     
+    
+    
