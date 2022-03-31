2adad8566818bb17736d8885929fdb835e34ca73
JoannaBusmail <joanna.busmail@gmail.com>
2022-03-31 17:56:51 +0200

    add jsclasses palette and functions - it'sworking

--- src/js/color.js
+++ src/js/color.js
@@ -1,3 +1,30 @@
 'use strict';
 
-const cardContainer = document.querySelector('.js_preview');
+const cardContainer = document.querySelector('.js_cardContainer');
+const radioButtonBlue = document.querySelector ('.js_button_paletteBlue');
+const radioButtonRed =document.querySelector ('.js_button_paletteRed');
+const radioButtonMix = document.querySelector ('.js_button_paletteMix');
+
+
+
+function changeColorRed (event){
+    cardContainer.classList.remove ('paletteBlue');
+    cardContainer.classList.remove ('paletteMix');
+    cardContainer.classList.add ('paletteRed');
+}
+function changeColorMix (event){
+    cardContainer.classList.remove ('paletteBlue');
+    cardContainer.classList.remove ('paletteRed');
+    cardContainer.classList.add ('paletteMix');
+}
+function changeColorBlue (event){
+    cardContainer.classList.remove ('paletteRed');
+    cardContainer.classList.remove ('paletteMix');
+    cardContainer.classList.add ('paletteBlue');
+}
+
+
+
+radioButtonBlue.addEventListener ('click' , changeColorBlue );
+radioButtonRed.addEventListener ('click' , changeColorRed );
+radioButtonMix.addEventListener ('click' , changeColorMix );
