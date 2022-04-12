9305ccd8267ce4937c61777993aec83786a8de85
PradoCarretero <p.carreteroayala@gmail.com>
2022-04-12 19:11:06 +0200

    save general probelms

--- src/js/3_color.js
+++ src/js/3_color.js
@@ -1,41 +1,38 @@
-'use strict';
+"use strict";
 
 /*const cardContainer = document.querySelector('.js_cardContainer');
 const radioButtonBlue = document.querySelector ('.js_button_paletteBlue');
 const radioButtonRed =document.querySelector ('.js_button_paletteRed');
 const radioButtonMix = document.querySelector ('.js_button_paletteMix');*/
 
-function elementsHtml ( classes){
-const element = document.querySelector (classes);
-return element;
+function elementsHtml(classes) {
+  const element = document.querySelector(classes);
+  return element;
 }
-const cardContainer = elementsHtml ('.js_cardContainer');
-const radioButtonRed = elementsHtml  ('.js_button_paletteRed');
-const radioButtonMix = elementsHtml  ('.js_button_paletteMix');
-const radioButtonBlue = elementsHtml  ('.js_button_paletteBlue');
+const cardContainer = elementsHtml(".js_cardContainer");
+const radioButtonRed = elementsHtml(".js_button_paletteRed");
+const radioButtonMix = elementsHtml(".js_button_paletteMix");
+const radioButtonBlue = elementsHtml(".js_button_paletteBlue");
 
-
-
-function changeColorRed (event){
-    cardContainer.classList.remove ('paletteBlue');
-    cardContainer.classList.remove ('paletteMix');
-    cardContainer.classList.remove ('paletteRed');
-    cardContainer.classList.add ('paletteRed');
+function changeColorRed(event) {
+  cardContainer.classList.remove("paletteBlue");
+  cardContainer.classList.remove("paletteMix");
+  cardContainer.classList.remove("paletteRed");
+  cardContainer.classList.add("paletteRed");
 }
-function changeColorMix (event){
-    cardContainer.classList.remove ('paletteBlue');
-    cardContainer.classList.remove ('paletteRed');
-    cardContainer.classList.remove ('paletteMix');
-    cardContainer.classList.add ('paletteMix');
+function changeColorMix(event) {
+  cardContainer.classList.remove("paletteBlue");
+  cardContainer.classList.remove("paletteRed");
+  cardContainer.classList.remove("paletteMix");
+  cardContainer.classList.add("paletteMix");
 }
-function changeColorBlue (event){
-    cardContainer.classList.remove ('paletteRed');
-    cardContainer.classList.remove ('paletteMix');
-    cardContainer.classList.remove ('paletteBlue');
-    cardContainer.classList.add ('paletteBlue');
+function changeColorBlue(event) {
+  cardContainer.classList.remove("paletteRed");
+  cardContainer.classList.remove("paletteMix");
+  cardContainer.classList.remove("paletteBlue");
+  cardContainer.classList.add("paletteBlue");
 }
 
-
-radioButtonBlue.addEventListener ('click' , changeColorBlue );
-radioButtonRed.addEventListener ('click' , changeColorRed );
-radioButtonMix.addEventListener ('click' , changeColorMix );
+radioButtonBlue.addEventListener("click", changeColorBlue);
+radioButtonRed.addEventListener("click", changeColorRed);
+radioButtonMix.addEventListener("click", changeColorMix);
